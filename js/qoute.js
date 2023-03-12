ac593befe82c3a0d211930c67bbd9f3be130299e
KALs3 <dylanzed8@gmail.com>
2023-03-12 08:30:36 +0200

    first commit

---
+++ js/qoute.js
@@ -0,0 +1,13 @@
+const qouteText = document.querySelector(".quote"),
+authorName= document.querySelector(".author")
+qouteBtn = document.querySelector(".new-qoute-btn");
+
+function randomQoute(){
+    fetch('https://api.quotable.io/random').then(res => res.json()).then(result => {
+        console.log(result)
+        qouteText.innerHTML = result.content;
+        authorName.innerHTML = result.author;
+    })
+}
+
+qouteBtn.addEventListener("click",randomQoute)
