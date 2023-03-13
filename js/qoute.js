3baf60d1b4d04c9011d6311cffe40b46f0f34c6d
KALs3 <dylanzed8@gmail.com>
2023-03-13 10:14:41 +0200

    onreload a new quote is generated directly

--- js/qoute.js
+++ js/qoute.js
@@ -1,7 +1,7 @@
 const qouteText = document.querySelector(".quote"),
 authorName= document.querySelector(".author")
 qouteBtn = document.querySelector(".new-qoute-btn");
-
+window.onload = randomQoute();
 function randomQoute(){
     fetch('https://api.quotable.io/random').then(res => res.json()).then(result => {
         console.log(result)
